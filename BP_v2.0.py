843f02ec40f4c513866faba70ed7d0889bef30a9
FURY <79848348+FURY312@users.noreply.github.com>
2022-10-22 20:26:08 +0900

    Update BP_v2.0.py

--- BP_v2.0.py
+++ BP_v2.0.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
-df = pd.read_csv('test.csv')
+fi = pd.read_csv('test.csv')
 
-num_of_cases = df.groupby('Classe')['rv'].max()
+num_of_cases = fi.groupby('Classe')['rv'].max()
 n_top = num_of_cases.nlargest(10).index
 
 from benfordslaw import benfordslaw
 def benford(classe, pos):
-    global df
-    df_n = df[df.Classe == classe]
-    data = df_n['basic']
+    global fi
+    fi_n = fi[fi.Classe == classe]
+    data = fi_n['basic']
     bl = benfordslaw(alpha=0.05, method='ks', pos=pos, verbose=3)
     results = bl.fit(data) # 분석
     bl.plot(title=classe+ ', pos: '+str(pos)) #출력
